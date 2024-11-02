9c88d810154dda1b8d7492c6ae8a7cbcdd9fa157
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-02 22:29:00 +0100

    costum error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/ui/button/button.svelte';
+    import Pulse from '$lib/components/ui/loaders/Pulse.svelte';
+</script>
+
+<div class="flex flex-col w-full h-svh items-center justify-center">
+	<h1 class="text-3xl mb-6">Oops! That didn’t work...</h1>
+	<Button on:click={() => goto('/')}>Go back Home</Button>
+    <div class="mt-10">
+        <Pulse/>
+    </div>
+</div>
