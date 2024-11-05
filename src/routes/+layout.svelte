c54406a89e05fef09ae5525f3a27618f45d7128d
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-05 17:38:02 +0100

    Add repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import Moon from 'lucide-svelte/icons/moon';
 	import { toggleMode } from 'mode-watcher';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import { Github } from 'lucide-svelte';
 
 </script>
 
@@ -16,13 +17,20 @@
 	<slot />
 </div>
 
-<Button on:click={toggleMode} variant="outline" size="icon" class="fixed top-2 right-2 z-20">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span class="sr-only">Toggle theme</span>
-</Button>
+<div class="flex flex-col gap-2 fixed top-2 right-2 z-20">
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+	<a href="https://github.com/Lukas-Siegle/Personal-Website">
+		<Button variant="outline" size="icon">
+			<Github/>
+		</Button>
+	</a>	
+</div>
 
