5038cb821ae1c923890ab884d6a6f5c053e6f6da
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-06 23:45:34 +0100

    copy button

---
+++ src/lib/components/ui/copy-button/button.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Button from '../button/button.svelte';
+	import { toast } from 'svelte-sonner';
+	import { Copy } from 'lucide-svelte';
+
+	export let content = '';
+	export let style = '';
+
+	async function copyToClipboard() {
+		try {
+			await navigator.clipboard.writeText(content);
+			toast('Copied to Clipboard!');
+		} catch (error) {
+			toast('Copy failed');
+		}
+	}
+</script>
+
+<Button
+	on:click={copyToClipboard}
+	variant="ghost"
+	size="icon"
+	class={`${style} transition-colors hover:bg-white/25`}
+>
+	<Copy color="yellow" size=18/>
+</Button>
