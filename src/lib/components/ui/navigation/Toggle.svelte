16a3ed572ec4eda097209c0ede09245478e9e0a3
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-08 12:30:02 +0100

    refactor

--- src/lib/components/ui/navigation/Toggle.svelte
+++ src/lib/components/ui/navigation/Toggle.svelte
@@ -16,10 +16,10 @@
 	</Button>
 	<Dialog.Root>
 		<Dialog.Trigger>
-		  <Button variant="outline" size="icon">
+		  <div class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 w-10">
 			<Cookie />
 			<span class="sr-only">Cookie Information</span>
-		  </Button>
+		  </div>
 		</Dialog.Trigger>
 		<Dialog.Content class="sm:max-w-[425px]">
 		  <Dialog.Header>
